9b82dba9c89751434cecdfc77c90af6773782927
cheloniamydasCalmsea <cheloniamydasCalmsea@gmail.com>
2023-12-09 20:41:20 +0900

    genesis

---
+++ src/main/java/com/bada/sea/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.bada.sea.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/hello")
+	public String getJsonResponse() {
+        // JSON 데이터 생성
+        String jsonString = "{\"say\":\"hello\"}";
+
+        return jsonString;
+    }
+}
